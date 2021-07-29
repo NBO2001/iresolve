68cc18733f765c8b49c6db8e53ac27cac932d17b
NBO2001 <natanbezerr63@gmail.com>
2021-07-29 12:50:37 -0400

    Clear store when new question

--- src/pages/home/index.jsx
+++ src/pages/home/index.jsx
@@ -2,6 +2,7 @@ import React, { useEffect, useRef } from 'react'
 import api from '../../config';
 import { useDispatch } from 'react-redux';
 import { setQuestion } from '../../redux/modules/questions';
+import { setButton } from '../../redux/modules/buttons';
 import { ProblemHeard, ProblemOptions, ButtonOptions } from '../../components';
 import { Wrapper } from './style';
 
@@ -12,6 +13,8 @@ function Home() {
     const dispatch = useDispatch();
 
     const novaQuest = async () => {
+        dispatch(setQuestion());
+        dispatch(setButton());
         await api.get('/question').then( async function (res) {
             if(isMount){
                 const { data } = res;
